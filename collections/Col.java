af3e27440e19eeaba8c279f9ddacc06733766a93
raxitzora <raxitzora@gmail.com>
2026-03-05 12:15:21 +0530

    collections completed

---
+++ collections/Col.java
@@ -0,0 +1,19 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class Col {
+    public static void main(String[] args) {
+
+    ArrayList<Integer> list = new ArrayList<>();
+
+    for(li:list){
+     
+    }
+  
+
+
+
+
+    }
+}
