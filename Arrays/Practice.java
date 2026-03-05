dc259d142485083c3d7e14af847cf57b62a28058
raxitzora <raxitzora@gmail.com>
2026-03-05 11:38:52 +0530

    Arrays completed

--- Arrays/Practice.java
+++ Arrays/Practice.java
@@ -1,15 +1,25 @@
 
 public class Practice {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
+        // String[] names = {"raxit","ramesh","suresh","rajesh"};
+    
+        // for (String name : names) {
+        //     System.out.println(name);
+            
+        // }
 
+    //    int[] number = {1,2,3,4,5,6,7,8};
 
+    //    System.out.println(number.length);
 
+    int[] marks = {85, 90, 78, 92};
 
-
-        
-        System.out.println(a+b);
+    for (int mark:marks){
+        System.out.println(mark);
     }
+
+    
+    }
+
     
 }
