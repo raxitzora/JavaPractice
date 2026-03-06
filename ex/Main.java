862025211d6333d71ae8046e8b58b01a723d4565
raxitzora <raxitzora@gmail.com>
2026-03-06 11:50:35 +0530

    fundamentals

---
+++ ex/Main.java
@@ -0,0 +1,16 @@
+package ex;
+
+public class Main {
+    public static void main(String[] args) {
+  int number = 11;
+
+  if(number%2==0){
+    System.out.println("Number is even");
+  }
+  else{
+    System.out.println("Number is odd");
+  }
+        
+    }
+    
+}
