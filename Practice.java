0fc63a7a7c1598837455d538f80008e878a8c36b
raxitzora <raxitzora@gmail.com>
2026-03-11 23:26:36 +0530

    java

--- Practice.java
+++ Practice.java
@@ -3,11 +3,13 @@ import java.util.List;
 
 public class Practice {
 
-    
+
     public static void main(String[] args) {
 
 
         List<Integer> list = new ArrayList<>();
+
+        
         
         list.add(5);
 
