ecad5960c78c22bc5b5e1a5d0840c6d2aee006d1
raxitzora <raxitzora@gmail.com>
2026-03-11 23:27:21 +0530

    java

--- Practice.java
+++ Practice.java
@@ -7,13 +7,16 @@ public class Practice {
     public static void main(String[] args) {
 
 
+
+        
+
         List<Integer> list = new ArrayList<>();
 
 
         
 
 
-        
+
         list.add(5);
 
 
