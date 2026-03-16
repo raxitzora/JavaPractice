fc065d66eb08ac41c585078e8c09793af960e53b
Raxit Zora <raxitzora@gmail.com>
2026-03-16 22:45:19 +0530

    time and space complexity

---
+++ DSA/SpaceTimeComplexity/Constant.java
@@ -0,0 +1,12 @@
+package DSA.SpaceTimeComplexity;
+
+public class Constant {
+    public static void main(String[] args) {
+
+        int[] numbers = {1,2,3,4,5};
+
+       numbers[0] = numbers[0]+10;
+       System.out.println(numbers);
+
+    }
+}
---
+++ DSA/SpaceTimeComplexity/Main.java
@@ -0,0 +1,11 @@
+package DSA.SpaceTimeComplexity;
+
+public class Main {
+    public static void main(String[] args) {
+        //BigO(1)
+    //     int[] arr = new int[]{1,2,3,4,5};
+
+    //     System.out.println(arr[0]);
+    // 
+    }
+}
