5d7ab0155ca476126902788abe856a965875fb57
Raxit Zora <raxitzora@gmail.com>
2026-03-17 00:56:54 +0530

    All complexities

--- DSA/SpaceTimeComplexity/AllComplexities.java
+++ DSA/SpaceTimeComplexity/AllComplexities.java
@@ -29,10 +29,11 @@ public class AllComplexities {
         for(int i = 0; i < n; i++){
 
             for(int j = 0; j < n; j++){
+                
                 for(int k = 0; k < n; k++){
                     System.out.println(i + " " + j + " " + k);
                 }
-                
+
             }
         }
 
