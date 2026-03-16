4b82a0b792e8b3ddab8202b46a1ccb2d367af286
Raxit Zora <raxitzora@gmail.com>
2026-03-17 00:47:59 +0530

    All complexities

---
+++ DSA/SpaceTimeComplexity/AllComplexities.java
@@ -0,0 +1,38 @@
+package DSA.SpaceTimeComplexity;
+
+public class AllComplexities {
+    public static void main(String[] args) {
+
+        
+        //O(1) constant time = Input size बढ़े या घटे, operation same रहता है.
+        int[] arr = {1,2,3,4,5};
+        
+
+        //O(n) Linear time = Input size jitna bada, loop utni baar chalega.
+        int[] arr2 = {1,2,3,4,5};
+        for(int i=1;i<arr2.length;i++){
+            System.out.println(arr2[i]);
+        }
+
+        //O(n^2) Quadratic time = Do nested loops.
+        int n =5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.println(i+j);
+            }
+        }
+
+
+        //O(n^3) Cubic time = Teen nested loops.
+         int h = 5;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                for(int k = 0; k < n; k++){
+                    System.out.println(i + " " + j + " " + k);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ DSA/SpaceTimeComplexity/Prac.java
@@ -0,0 +1,20 @@
+package DSA.SpaceTimeComplexity;
+
+import java.util.ArrayList;
+
+public class Prac {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> number = new ArrayList<>();
+        number.add(4);
+        number.add(3);
+        System.out.println(number);
+
+        
+
+        
+    }
+    
+    
+    
+}
