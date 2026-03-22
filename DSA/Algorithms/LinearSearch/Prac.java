bbad41c6b54dd192614d1a1ef53f16f5c5c73b43
raxitzora <raxitzora@gmail.com>
2026-03-22 23:32:58 +0530

    functions

--- DSA/Algorithms/LinearSearch/Prac.java
+++ DSA/Algorithms/LinearSearch/Prac.java
@@ -8,8 +8,6 @@ public class Prac {
         int ans = LinearSearchh(numbers,target);
     System.out.println("Element is at index of: "+ans);
 
-
-
     }
 
     static int LinearSearchh(int[] arr, int target){
