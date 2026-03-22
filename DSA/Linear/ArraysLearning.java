a80af7288241e999d66ae5b0d5863fe33f47954a
raxitzora <raxitzora@gmail.com>
2026-03-22 23:58:06 +0530

    arrays sum

--- DSA/Linear/ArraysLearning.java
+++ DSA/Linear/ArraysLearning.java
@@ -5,11 +5,10 @@ public class ArraysLearning {
         
 
         //total sum of array
-        
+
         // int[] arr = {5, 10, 15, 20};
 
         // int sum = 0;
-
     
         // for(int i =0;i<arr.length;i++){
         // sum = sum + arr[i];
