a48605d8397fb2601cc919ab563742f9af950044
raxitzora <raxitzora@gmail.com>
2026-03-22 23:57:05 +0530

    arrays sum

--- DSA/Linear/ArraysLearning.java
+++ DSA/Linear/ArraysLearning.java
@@ -3,18 +3,39 @@ package DSA.Linear;
 public class ArraysLearning {
     public static void main(String[] args) {
         
-        int[] arr = {1,2,3,4,5,6,7};
 
-        // for(int i =0;i<arr.length;i++){
-        //     System.out.println(arr[i]);
-        // }
+        //total sum of array
+        // int[] arr = {5, 10, 15, 20};
 
         // int sum = 0;
 
         // for(int i =0;i<arr.length;i++){
+        // sum = sum + arr[i];
+        // }
+        // System.out.println("Total sum is "+sum);    
+
+
+
+        // simple loop
+        // for(int i =0;i<arr.length;i++){
+        //     System.out.println(arr[i]);
+        // }
+
+        //Sum of numbers
+        // int sum = 0;
+        // for(int i =0;i<arr.length;i++){
         //     sum = sum + arr[i];
         // }
         // System.out.println(sum);
+        
+
+        //Searching
+        // int target = 6;
+        // for(int i = 0;i<arr.length;i++){
+        //     if(arr[i]==target){
+        //         System.out.println("Found at index"+i);
+        //     }
+        // }
 
     
       
