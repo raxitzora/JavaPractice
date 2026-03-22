cbfaf967c4cfa8dea6812230c9f734e04750791a
raxitzora <raxitzora@gmail.com>
2026-03-23 00:08:23 +0530

    Practice

--- DSA/Algorithms/LinearSearch/Prac.java
+++ DSA/Algorithms/LinearSearch/Prac.java
@@ -12,6 +12,7 @@ public class Prac {
     static int LinearSearchh(int[] arr, int target){
         if(arr.length==0){
             return -1;
+            
         }
         for(int i =0;i<=arr.length;i++){
             if(arr[i]==target){
