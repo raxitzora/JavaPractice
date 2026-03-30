4ba6641625060879f41084d77e7bedd8e06978dc
raxitzora <raxitzora@gmail.com>
2026-03-30 23:04:38 +0530

    sets completed

--- collections/Practice.java
+++ collections/Practice.java
@@ -2,9 +2,11 @@ package collections;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Practice {
     public static void main(String[] args) {
@@ -24,9 +26,29 @@ public class Practice {
         na.add("rax");
         na.add("raxi");
 
-        for (String name : na) {
-            System.out.println(name);
+        Set<Integer> nums = new LinkedHashSet<>();
+
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(1);
+
+        System.out.println(nums);
+
+        Set<String> names = new HashSet<>();
+
+        Set<Integer> numbers = new TreeSet<>();
+
+        numbers.add(40);
+        numbers.add(20);
+        numbers.add(30);
+    
+        for(int a:numbers){
+            System.out.println(a);
         }
+
+
+
         
 
 
