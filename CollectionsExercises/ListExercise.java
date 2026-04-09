27032bef9638e9d2ae2292e450426b1ff2f7c392
raxitzora <raxitzora@gmail.com>
2026-04-09 12:45:58 +0530

    users

---
+++ CollectionsExercises/ListExercise.java
@@ -0,0 +1,25 @@
+
+package CollectionsExercises;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListExercise{
+    public static void main(String[] args){
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(4);
+        list.set(1,2);
+
+
+        List<String> users = new ArrayList<>();
+
+        users.add("raxit");
+        users.add("amit");
+        users.add("bob");
+
+       
+
+       
+    }
+
+}
