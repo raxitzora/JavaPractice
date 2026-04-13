554f11bf327b6e7de16620730c99ceaf7f1b50d4
raxitzora <raxitzora@gmail.com>
2026-04-14 01:25:45 +0530

    inheritence

---
+++ oops/inheritence/Test.java
@@ -0,0 +1,24 @@
+package oops.inheritence;
+
+class Animal{
+    public void walk(){
+        System.out.println("Animal is walking");
+    }
+
+}
+
+
+ class Dog extends Animal{
+    public void bark(){
+        System.out.println("barking......");
+    }
+    
+}
+
+class Test{
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.bark();
+        d.walk();
+    }
+}
