fc3c08a88ef360d7f1374f7dbf032c3518d9b0c6
raxitzora <raxitzora@gmail.com>
2026-05-01 13:49:30 +0530

    DSA

---
+++ patterns/Fifth.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class Fifth {
+     
+    public static void main(String[] args) {
+        for(int i=5;i>=1;i--){
+            for(int j = 5;j<=5-1;j++){
+                System.out.print(i);
+            }
+            System.out.println("");
+        }
+
+    }
+}
---
+++ prac/Prac.java
@@ -0,0 +1,26 @@
+package prac;
+
+import java.util.ArrayList;
+
+public class Prac {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5};
+
+
+        for(int i =0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+
+        
+
+
+    
+
+
+
+
+    }
+    
+}
