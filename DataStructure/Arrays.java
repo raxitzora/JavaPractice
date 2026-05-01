530efea26d2bd9300cd94e26effe4cac9feb98df
raxitzora <raxitzora@gmail.com>
2026-05-01 13:55:19 +0530

    arrays

---
+++ DataStructure/Arrays.java
@@ -0,0 +1,23 @@
+package DataStructure;
+
+public class Arrays {
+    public static void main(String[] args) {
+            String[] billPayments ={"IT","Account","Cloud"};
+            int [] num = {1,2,3};
+
+            System.out.println(num);
+
+            String[] cars = {"eon","suzuki"};
+
+            for(String c:cars){
+                System.out.println(c);
+            }
+
+     
+        
+    }
+
+    
+    
+    
+}
