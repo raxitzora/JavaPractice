abd39dd0ddecbcc0d529cebcdff8b9945f3abd4a
raxitzora <raxitzora@gmail.com>
2026-05-21 01:21:41 +0530

    Linked list completed

--- DataStructure/LinkedListExample.java
+++ DataStructure/LinkedListExample.java
@@ -1,10 +1,26 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class LinkedListExample {
     public static void main(String[] args) {
+
+        ArrayList<Integer> num = new ArrayList<>();
+        num.add(2);
+        num.addFirst(22);
+
+
+
+
         LinkedList<Integer> numbers = new LinkedList<>();
         numbers.add(4);
         numbers.add(3);
+        numbers.addFirst(11);
+        numbers.addLast(22);
+
+        numbers.push(333);
+        numbers.pop();
+
+      
         
 
         System.out.println(numbers);
