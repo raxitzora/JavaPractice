1ecba949cf6709a82a80a79768dce74a136efce8
raxitzora <raxitzora@gmail.com>
2026-05-21 01:12:45 +0530

    linkedlist started

---
+++ DataStructure/LinkedListExample.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        LinkedList<Integer> numbers = new LinkedList<>();
+        numbers.add(4);
+        numbers.add(3);
+        
+
+        System.out.println(numbers);
+    }
+}
