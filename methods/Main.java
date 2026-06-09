62c2c833ae74f307e029d4eeab254c6ad206251e
raxitzora <raxitzora@gmail.com>
2026-06-09 16:56:47 +0530

    methods starts

---
+++ methods/Main.java
@@ -0,0 +1,22 @@
+package methods;
+public class Main {
+
+    public static int sum(){
+        int a = 5, b = 5;
+        int total = a +b;
+        return total;
+    }
+
+
+
+
+  
+    public static void main(String[] args) {
+
+        int full = sum();
+        System.out.println(full);
+
+
+        
+    }
+}
